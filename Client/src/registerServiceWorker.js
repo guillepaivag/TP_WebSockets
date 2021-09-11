b4a3b9608fb045d8357ad91c9d0981d38c2f3e16
arturojeich <arturojeich@gmail.com>
2021-09-11 11:49:41 -0400

    Agregado, examen parcial

--- Client/src/registerServiceWorker.js
+++ Client/src/registerServiceWorker.js
@@ -23,7 +23,7 @@ if (process.env.NODE_ENV === 'production') {
       console.log('New content is available; please refresh.')
     },
     offline () {
-      console.log('No internet connection found. App is running in offline mode.')
+      console.log('No internet connection found. App running in offline mode.')
     },
     error (error) {
       console.error('Error during service worker registration:', error)
